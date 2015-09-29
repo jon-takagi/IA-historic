b36507924103054b5ae2258001dc8fb0e59ef7a8
jon.takagi <40095@jisedu.or.id>
2015-09-29 20:55:58 +0700

    Added coloring based on status & seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+project = Project.create(name: "CS IA")
+project.success_conditions.build(description: "Planning is 6/6 ")
+project.success_conditions.build(description: "Solution overview is 6/6 ")
+project.success_conditions.build(description: "Development is 12/12 ")
+project.success_conditions.build(description: "Functionality is 4/4 ")
+project.success_conditions.build(description: "Evaluation is 6/6 ")
+
+project.tasks.build(description: "Planning", status: 2)
+project.tasks.build(description: "Solution overview", status: 1)
+project.tasks.build(description: "Development", status: 3)
+project.tasks.build(description: "Functionality", status: 3)
+project.tasks.build(description: "Evaluation", status: 0)
+
+project.save
